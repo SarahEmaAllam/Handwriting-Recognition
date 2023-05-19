f5dffbe674c8d913ae8ba5ca2fcd0682289a0990
vyvytn <vyvy.tns@gmail.com>
2023-05-19 12:38:30 +0200

    set up

---
+++ Augmentation/LineAugmentation.py
@@ -0,0 +1,16 @@
+import numpy as np
+import imgaug as ia
+import imgaug.augmenters as iaa
+import cv2
+from wezel.main import assert_dir
+
+ia.seed(1)
+
+
+def read_images(path):
+    srcimage = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+
+seq = iaa.Sequential([
+
+])
