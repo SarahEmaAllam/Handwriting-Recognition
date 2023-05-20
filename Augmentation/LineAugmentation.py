48d26a30f7c87ff094bde8648877ed7394a31422
tranngocvyvy <vyvy.tns@gmail.com>
2023-05-20 11:55:12 +0200

    rotating list of images; multiple lines still need to be developed

--- Augmentation/LineAugmentation.py
+++ Augmentation/LineAugmentation.py
@@ -1,3 +1,5 @@
+import random
+
 import imageio
 import numpy as np
 import imgaug as ia
@@ -7,6 +9,7 @@ import cv2
 import albumentations as A
 from wezel.main import assert_dir
 from PIL import Image
+import os
 
 ia.seed(1)
 
@@ -24,26 +27,55 @@ def read_images(path):
     transformed_image = transformed["image"]
 
 
+def rotate_by_degree(img_list):
+    nr = len(img_list)
+    print(nr)
+    degree_diff = 90 / nr
+    degree_start = -degree_diff
+    degree_end=0
+    aug_img_list = []
+    for img in img_list:
+        rotate = iaa.Affine(rotate=(degree_start, degree_end), mode='constant')
+        image_aug = rotate(image=img)
+        ia.imshow(image_aug)
+        aug_img_list.append(image_aug)
+        print('start', degree_start)
+        print('end', degree_end)
+        degree_start = degree_end
+        degree_end += degree_diff
+        aug_img_list.append(image_aug)
+
+    ia.imshow(np.hstack(aug_img_list))
+
+
+def random_rotation_by_list(path):
+    # choose a random directory and file
+    random_file = random.choice(os.listdir(os.path.join(path)))
+
+    # read and show image
+    img = iio.imread(os.path.join(path, random_file))
+    img_list = []
+    for i in range(0, 4):
+        random_file = random.choice(os.listdir(os.path.join(path)))
+        img_list.append(random_file)
+    ia.seed(4)
+    aug_img_list = []
+    rotate = iaa.Affine(rotate=(-135, 135), mode='constant')
+
+    img_list = [img, img, img, img, img]
+    image_aug = rotate(images=img_list)
+    ia.imshow(np.hstack(image_aug))
+
+
 if __name__ == '__main__':
-    '''aug_img = read_images(
-        'navis-QIrug-Qumran_extr09_0001-line-008-y1=400-y2=515-zone-HUMAN-x=1650-y=0049-w=0035-h=0042-ybas=0027-nink=631-segm=COCOS5cocos.pgm')
-    #aug_img = aug_img.astype(np.uint8)
-    cv2.imshow('augmented', aug_img)
-    # show all images
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    '''
     # path = 'navis-QIrug-Qumran_extr09_0001-line-008-y1=400-y2=515-zone-HUMAN-x=1650-y=0049-w=0035-h=0042-ybas=0027-nink=631-segm=COCOS5cocos.pgm'
     # srcimage = cv2.imread(path, -1)
     # srcimage = srcimage.astype(np.uint8)
-    path = 'pics/navis-QIrug-Qumran_extr09_0001-line-008-y1=400-y2=515-zone-HUMAN-x=1650-y=0049-w=0035-h=0042-ybas=0027-nink=631-segm=COCOS5cocos.jpg'
-    im = iio.imread(path)
-    ia.imshow(im)
 
-    ia.seed(4)
+    path = 'ressources'
+    random_file = random.choice(os.listdir(os.path.join(path)))
 
-    rotate = iaa.Affine(rotate=(-25, 25))
-    image_aug = rotate(image=im)
-
-    print("Augmented:")
-    ia.imshow(image_aug)
+    img = iio.imread(os.path.join(path, random_file))
+    img_list = [img, img, img, img, img]
+    rotate_by_degree(img_list)
+    # ia.imshow(image_aug)
