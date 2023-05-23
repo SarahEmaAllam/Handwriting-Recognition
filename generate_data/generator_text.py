7c53c77fdee073a43d1d153e7fbd5c7b079bb11c
SarahEmaAllam <s.allam@student.rug.nl>
2023-05-23 12:27:22 +0200

    resized images for yolo, downscaled writing

--- generate_data/generator_text.py
+++ generate_data/generator_text.py
@@ -7,6 +7,7 @@ from text_generator_ngram import generator
 from typing import Union, Tuple
 import string
 import imgaug.augmenters as iaa
+from LineAugmentation import rotate
 
 # should be based on N-gram probability distribution
 FOLDER = 'train'
@@ -16,10 +17,10 @@ TEXT_LENGTH = 200 * np.random.randint(1, 5, size=1)[0]
 # SCRIPT_SIZE = 608
 NGRAM_SIZE = 4
 Box = [float, float, float, float]
-WIDTH = 1700
-HEIGHT = 1700
-PADDING = 50 * np.random.randint(5, size=1)[0]
-WHITESPACE = 30
+WIDTH = 640
+HEIGHT = 640
+PADDING = 10 * np.random.randint(5, size=1)[0]
+WHITESPACE = 10
 PATH = os.getcwd()
 SCRIPT_NAME = 'test3'
 LETTERS_FOLDER = os.path.join('..','preprocess', 'output', 'symbols')
@@ -65,7 +66,7 @@ def save_coco_label(file: str, label: str, points: Box, path: str):
     h = points[3]
     label = '{} {} {} {} {}'.format(label, x_c, y_c, w, h).replace('"', '')
     file = str(file) + '.txt'
-    with open(os.path.join(str(path), "labels", str(file)), 'a') as f:
+    with open(os.path.join("..","data", "labels", FOLDER, str(file)), 'a') as f:
         f.write(label)
         f.write("\n")
         f.close()
@@ -142,7 +143,7 @@ def draw_boxes(img, x, y, w, h):
     cv2.waitKey(0)
 
 
-def stitch(images, text, FOLDER, SCRIPT_NAME):
+def stitch(images, text, folder, script_name):
     """
     Create a new image for the script with fixed size. Start adding from right to left
     the selected letters from the generated text. When PADDING is reached in width (x-axis), make
@@ -159,22 +160,25 @@ def stitch(images, text, FOLDER, SCRIPT_NAME):
     max_height = max(heights)
     new_im = Image.new('RGB', (WIDTH, HEIGHT), color="white")
 
-    x_offset = PADDING * np.random.randint(1, 5, size=1)[0]
+    x_offset = PADDING * np.random.randint(1, 4, size=1)[0]
     start_offset = x_offset
     y_offset = PADDING * np.random.randint(1, 2, size=1)[0]
     for idx, im in enumerate(images_type):
+        print("before ", im.size)
+        if im.size[0] > 1 and im.size[1] > 1:
+            im = im.resize((int(im.size[0] / 2), int(im.size[1] / 2)))
+            print(im.size)
         if new_im.size[0] - (x_offset + im.size[0]) <= start_offset:
-            y_offset = y_offset + int(
-                (max_height - im.size[1]) / 2) + max_height
+            y_offset = y_offset + int(max_height / 2)
             x_offset = start_offset
         else:
             #  CHANGE Y OFFSET BASED ON (NEW_IMG.HEIGTH - IM.HEIGTH) / 2
             y_offset = y_offset
             # (x_offset, 0) = upper left corner of the canvas where to paste
-        cropping = np.random.randint(15, 20, size=1)[0]
-        if im.size[0] > 30:
+        cropping = np.random.randint(7 , 10, size=1)[0]
+        if im.size[0] > 10:
             im = im.crop((cropping, 0, im.size[0] - cropping, im.size[1]))
-        new_im.paste(im, (new_im.size[0] - (x_offset + im.size[0]), y_offset))
+        new_im.paste(im, (new_im.size[0] - x_offset, y_offset))
         w = im.size[0]
         h = im.size[1]
         x_c = (new_im.size[0] - (x_offset + w / 2)) / WIDTH
@@ -185,14 +189,14 @@ def stitch(images, text, FOLDER, SCRIPT_NAME):
         label = text[idx]
         # skip punctuations, do not save their labels, yolo should not learn them
         if label not in string.punctuation:
-            save_coco_label(SCRIPT_NAME, label, box, PATH)
+            save_coco_label(script_name, label, box, PATH)
             # uncomment ot draw boxes
             # draw_boxes(new_im, x_c, y_c, w, h)
         # uncomment to see the process of stitching
         # new_im.show()
         #  slide the upper left corner for pasting next image next iter
         x_offset = x_offset + im.size[0] + cropping
-    new_im.save(os.path.join('data', FOLDER, SCRIPT_NAME + '.png'))
+    new_im.save(os.path.join('..','data', 'images', folder, script_name + '.png'))
 
 def transform_letter(image: Image.Image) -> Image.Image:
     """
@@ -245,9 +249,18 @@ def sample_text_generator(TEXT_LENGTH, NGRAM_SIZE):
     script = np.array(script)
     return script, text
 
-def generate_sample(FOLDER, SCRIPT_NAME):
-    # class_names = glob(LETTERS_FOLDER + os.sep+ "*", recursive=False)
+def generate_sample(folder, script_name):
+    class_names = glob(LETTERS_FOLDER + os.sep+ "*", recursive=False)
+    print(class_names)
+
     # images = load_classes(class_names)
     script, text = sample_text_generator(TEXT_LENGTH, NGRAM_SIZE)
-    stitch(script, text, FOLDER, SCRIPT_NAME)
 
+    # THIS IS VYVY'S PART
+    # for im in script:
+    #     print(im.shape)
+    # rotate([im for im in script if im.shape[0] > 20])
+
+    stitch(script, text, folder, script_name)
+
+generate_sample(FOLDER, SCRIPT_NAME)
