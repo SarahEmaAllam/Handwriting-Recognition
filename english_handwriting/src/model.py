7982a851adeea24c1ca2e26f8a36dfeb8f773b16
SarahEmaAllam <s.allam@student.rug.nl>
2023-06-09 19:12:12 +0200

    fixes until ctc loss

--- english_handwriting/src/model.py
+++ english_handwriting/src/model.py
@@ -38,19 +38,23 @@ def apply_twice_bn(x):
 class CTCLayer(keras.layers.Layer):
     def __int__(self, name=None):
         super().__init__(name=name)
-        self.loss_fn = tf.nn.ctc_loss
+        # self.loss_fn =  keras.backend.ctc_batch_cost
 
     def call(self, y_true, y_pred):
         print("y_true " , y_true)
         print("y_pred : " , y_pred)
-        batch_len = tf.cast(tf.shape(y_true[0]), dtype="int64")
-        input_length = tf.cast(tf.shape(y_pred)[1], dtype="float32")
+        batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
+        input_length = tf.cast(tf.shape(y_pred)[1], dtype="int64")
         label_length = tf.cast(tf.shape(y_true)[1], dtype="int64")
 
-        input_length = input_length * tf.ones(shape=(batch_len, 1), dtype="float32")
+        input_length = input_length * tf.ones(shape=(batch_len, 1), dtype="int64")
         label_length = label_length * tf.ones(shape=(batch_len, 1), dtype="int64")
-        loss = self.loss_fn(y_true, y_pred, input_length, label_length)
+        print("input_length : ", input_length)
+        print("label_length : ", label_length)
+        loss = keras.backend.ctc_batch_cost(y_true, y_pred, input_length, label_length)
         self.add_loss(loss)
+
+        # At test time, just return the computed predictions.
         return y_pred
 
 
@@ -97,6 +101,9 @@ class Model:
         output = apply_BLSTM(256, output)
         output = apply_BLSTM(256, output)
         print("bidire : ", output.shape)
+        output = keras.layers.Flatten()(output)
+        print("flat: ", output.shape)
+        # +2 is to account for the two special tokens introduced by the CTC loss.
         output = keras.layers.Dense(output_shape , activation="softmax")(output)
         print("final : ", output.shape)
         output = CTCLayer()(labels, output)
