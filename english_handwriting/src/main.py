ba2de94cd0f703ff77ee94c40f8a917777b7c8c1
tranngocvyvy <vyvy.tns@gmail.com>
2023-06-11 19:43:53 +0200

    main programm; generating for each line a txt file

--- english_handwriting/src/main.py
+++ english_handwriting/src/main.py
@@ -12,43 +12,47 @@ from tensorflow import keras
 from testing import decode_batch_predictions
 
 
-def load_model():
+# loads pre-trained and saved model from disk
+def load_model(model_path):
     set_working_dir(os.path.abspath(__file__))
-
-    trained_model_path = 'logs/trained_models/model_51--207.72'
-    # check if the path exists
-    if not os.path.exists(trained_model_path):
-        os.chdir('../')
-        # print the current working directory
-        print(os.getcwd())
-        # load the trained model
-    model = tf.keras.models.load_model(trained_model_path,
+    model = tf.keras.models.load_model(model_path,
                                        custom_objects={'Model': Model})
     return model
-    # print the model summary
-    # model.summary()
 
 
+# reads input from console; optional
 def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument("--p", "--path", action="store_true", help="Path to line image")
     args = parser.parse_args()
 
 
+# generates a txt file for each predicted line
+# saves file in results diretory
+# creates directoray if non-existent
 def text_to_file(text, name):
-    with open(name + '.txt', 'w') as f:
-        for line in text:
+    save_path = './results'
+    if os.path.exists(save_path) is False:
+        os.mkdir(save_path)
+    for idx, line in enumerate(text):
+        complete_path = os.path.join(save_path, name + str(idx + 1) + '.txt')
+        with open(complete_path, 'w') as f:
             f.write(f"{line}\n")
 
 
 if __name__ == '__main__':
+    MODEL_PATH = ''
     set_working_dir(os.path.abspath(__file__))
     parse()
     if len(sys.argv) > 1:
-        path = sys.argv[1]
+        try:
+            path = sys.argv[1]
+        except IndexError:
+            exit("Path couldn't be resolved.")
     else:
         path = input("Path to line image:")
-    model = load_model()
+
+    model = load_model(model_path=MODEL_PATH)
     proc_img, decoder = preprocess_test_images(path)
 
     prediction_model = keras.models.Model(
