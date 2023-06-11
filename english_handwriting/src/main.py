dbcf41455cdb367ed68a9d811f48597e327f8374
tranngocvyvy <vyvy.tns@gmail.com>
2023-06-11 19:17:21 +0200

    main programm; added txt file writing

--- english_handwriting/src/main.py
+++ english_handwriting/src/main.py
@@ -1,6 +1,6 @@
 import os
 from preprocessing.binarization import binarize
-from preprocessing.preprocessing import preprocess_test_data
+from preprocessing.preprocessing import preprocess_test_images
 
 from util.utils import set_working_dir
 from training import train
@@ -8,6 +8,8 @@ import sys
 import argparse
 import tensorflow as tf
 from model import Model
+from tensorflow import keras
+from testing import decode_batch_predictions
 
 
 def load_model():
@@ -33,6 +35,12 @@ def parse():
     args = parser.parse_args()
 
 
+def text_to_file(text, name):
+    with open(name + '.txt', 'w') as f:
+        for line in text:
+            f.write(f"{line}\n")
+
+
 if __name__ == '__main__':
     set_working_dir(os.path.abspath(__file__))
     parse()
@@ -41,7 +49,20 @@ if __name__ == '__main__':
     else:
         path = input("Path to line image:")
     model = load_model()
-    output, decoder = preprocess_test_data(path)
+    proc_img, decoder = preprocess_test_images(path)
+
+    prediction_model = keras.models.Model(
+        model.get_layer(name="image").input,
+        model.get_layer(name="dense").output
+    )
+
+    pred_batch = prediction_model.predict(proc_img)
+    predicted_text = []
+
+    for img in pred_batch:
+        predicted_line = decode_batch_predictions(pred_batch, decoder)
+        predicted_text.append(predicted_line)
+    text_to_file(predicted_text, path)
     # change working dir to root of project
     # os.chdir(os.path.dirname (os.path.abspath(__file__)))
     # train()
